7c43e691ca0f81acfefdd4cfe39c446604d06669
JuliaDemi123 <jdemitraszek@student.agh.edu.pl>
2024-10-18 17:41:54 +0200

    second commit to lab1!

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,7 +1,22 @@
 package agh.ics.oop;
 
 public class World {
+    public static void run(String[] messages)
+    {
+        System.out.println("Zwierzak idzie do przodu");
+        for(int i = 0; i < messages.length; i++)
+        {
+            System.out.print(messages[i]);
+            if (i < messages.length-1)
+            {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
-
+        System.out.println("system wystartowal");
+        run(args);
+        System.out.println("system zakonczyl dzialanie");
     }
 }
